c1accc59093188e70154907c8345624070cc0acc
Daria <daria7Vo@yandex.ru>
2021-03-10 12:02:11 +0300

    доработаны табы и добавлена секция с отзывами

--- src/js/script.js
+++ src/js/script.js
@@ -42,6 +42,23 @@ $(function() {
       .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active'); /* внутри общего блока container происходит поиск контента с классом catalog__content, у найденного контента класс активности удаляется, расчитывается номер активного блока (тот, на который нажали) и ему присваивается класс активности */
   });
 });
+
+  $('.catalog-item__link').each(function(i) { /* перебираем и находим все ссылки с данным классом, определяем на какую из сылок было нажатие */
+    $(this).on('click', function(e) { 
+      e.preventDefault(); /* при клике на ссылку с таким классом не должны выполняться действия по умолчанию */
+      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active'); /* при нажатии на ссылку с классом catalog-item__link класс активности у элемента catalog-item__content удаляется и класс активности присваивается другому элементу с классом catalog-item__list */
+      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    }) 
+  });
+
+  $('.catalog-item__back').each(function(i) { /* перебираем и находим все ссылки с данным классом */
+    $(this).on('click', function(e) { /* определяем на какую из ссылок было нажатие выполняем следующие действия */
+      e.preventDefault(); /* при клике на ссылку с таким классом не должны выполняться действия по умолчанию */
+      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active'); /* при нажатии на ссылку с классом catalog-item__back класс активности у элемента catalog-item__content удаляется и класс активности присваивается другому элементу с классом catalog-item__list */
+      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    }) 
+  })
+
 })(jQuery);
 
 
