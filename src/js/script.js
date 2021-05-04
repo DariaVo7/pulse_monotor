d418e3aab103faecfefde63b9981a78794594a21
Daria <daria7Vo@yandex.ru>
2021-05-04 16:56:06 +0300

    Вёрстка всех секций + карта + валидация форм

--- src/js/script.js
+++ src/js/script.js
@@ -59,6 +59,54 @@ $(function() {
     }) 
   })
 
+// Modal 
+
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow'); /* при нажатии на элемент с с data-атрибутом consultation, а также с таким же id, появляется затемнённый слой overlay и соответствующее модальное окно; в скобках указываем slow, чтобы слой появялся плавно */
+  });
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #order, #thanks').fadeOut('slow'); /* при нажатии на крестик, который имеет класс modal__close, затемнённый слой с соответсвующим модальным окном скрывается */
+  });
+  $('.button_mini').each(function(i) { /* команда each перебирает все элементы с классом button_mini, находит значение i того элемента, на который нажали, и дальше для этого элемента выполняется код. */
+    $(this).on('click', function () { /* this = тот элемент, на который нажали */
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text()); /* в модальное окно с id order, а именно в элемент с классом modal__descr выводится текст из элемента с классом catalog-item__subtitle */
+      $('.overlay, #order').fadeIn('slow'); /* при нажатии на элемент с классом botton_mini появляется затемнённый слой overlay и соответствующее модальное окно; в скобках указываем slow, чтобы слой появялся плавно */ 
+    })
+  });
+
+  function valideForms(form) { /* код, который можно подставлять, не прописывая заново */
+    $(form).validate({
+      rules: {
+        name: {
+          required: true, /* обязательное поле */
+          minlength: 2 /* минимальное кол-во символов = 2 */
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true /* плагин будет проверять корректно ли введён email */
+        }
+      },
+      messages: { /* Редактирование сообщений от плагина */
+        name: {
+          required: "Пожалуйста, введите своё имя",
+          minlength: jQuery.validator.format("Введите {0} символа")
+        },
+        phone: "Пожалуйста, введите номер телефона",
+        email: {
+          required: "Пожалуйста, введите свой почтовый адрес",
+          email: "Пожалуйста, введите потовый адрес в формате name@domain.com"
+        }
+      }
+    });
+  };
+
+  valideForms('#consultation-form');
+  valideForms('#consultation form');
+  valideForms('#order form');
+
+  $('input[name=phone]').mask("+7(999) 999-99-99");
+
 })(jQuery);
 
 
